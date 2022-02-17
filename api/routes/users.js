f51aad42198302b04896c08d0a109281e3c5a48e
Ilya Grachev <ilya095@gmail.com>
2022-02-17 17:11:08 +0300

    add logout route

--- api/routes/users.js
+++ api/routes/users.js
@@ -7,4 +7,10 @@ router.route('/signup').post(addUser);
 
 router.route('/signin').post(getUserByEmail);
 
+router.route('/logout').get((req, res) => {
+  req.session.destroy();
+  req.logout();
+  res.redirect('/');
+});
+
 export default router;
